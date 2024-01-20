5c3109e1b045b9de53f7e1d5a153aaf8641c71be
Erick Orlando <erickorlando@outlook.com>
2024-01-20 11:32:25 -0500

    Cambio de puerto

--- Client/Program.cs
+++ Client/Program.cs
@@ -6,6 +6,7 @@ var builder = WebAssemblyHostBuilder.CreateDefault(args);
 builder.RootComponents.Add<App>("#app");
 builder.RootComponents.Add<HeadOutlet>("head::after");
 
+// Esto sale porque la URL base del cliente es la misma que la del servidor
 builder.Services.AddScoped(sp => new HttpClient { BaseAddress = new Uri(builder.HostEnvironment.BaseAddress) });
 
 await builder.Build().RunAsync();
